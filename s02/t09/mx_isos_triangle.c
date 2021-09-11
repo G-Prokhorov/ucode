5401c993803a2cfcb6ad35c6f424576735a65e20
Gregory Prokhorov <gprokhorov@freshcode.org>
2021-09-11 17:13:30 +0300

    s02 t09

---
+++ s02/t09/mx_isos_triangle.c
@@ -0,0 +1,18 @@
+#include "mx_printchar.c"
+
+void mx_isos_triangle(unsigned int length, char c)
+{
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            mx_printchar(c);
+        }
+        mx_printchar('\n');
+    }
+}
+
+int main(void)
+{
+    mx_isos_triangle(3, '*');
+}
